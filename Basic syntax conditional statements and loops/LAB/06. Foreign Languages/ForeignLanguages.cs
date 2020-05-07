f0e361e95283ee9563f8d2a7385a62270df1d569
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-07 22:00:16 +0300

    Add ForeignLanguages

---
+++ Basic syntax conditional statements and loops/LAB/06. Foreign Languages/ForeignLanguages.cs
@@ -0,0 +1,27 @@
+﻿namespace _06._Foreign_Languages
+{
+    using System;
+
+    public class ForeignLanguages
+    {
+        public static void Main()
+        {
+            string Country = Console.ReadLine();
+            switch (Country)
+            {
+                case "England":
+                case "USA":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
