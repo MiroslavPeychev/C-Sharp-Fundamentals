398fddb8e9f7b3deb25d8e2375174237b7f80612
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-07 22:02:45 +0300

    Add DivisibleByThree

---
+++ Basic syntax conditional statements and loops/LAB/08. Divisible by 3/DivisibleByThree.cs
@@ -0,0 +1,23 @@
+﻿namespace _08._Divisible_by_3
+{
+    using System;
+
+    public class DivisibleByThree
+    {
+        public static void Main()
+        {
+            for (int i = 3; i < 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+        }
+    }
+}
