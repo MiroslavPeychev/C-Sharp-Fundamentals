9ea23bdcc3410a60a8e20473787d780f3b4c0e06
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-07 21:55:59 +0300

    Add StudentInformation

---
+++ Basic syntax conditional statements and loops/LAB/01. Student Information/StudentInformation.cs
@@ -0,0 +1,16 @@
+﻿namespace _01._Student_Information
+{
+    using System;
+
+    public class StudentInformation
+    {
+        public static void Main()
+        {
+            string studentName = Console.ReadLine();
+            int studentAge = int.Parse(Console.ReadLine());
+            double studentGrade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {studentName}, Age: {studentAge}, Grade: {studentGrade:F2}");
+        }
+    }
+}
