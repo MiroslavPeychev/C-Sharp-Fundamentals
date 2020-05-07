ebed24e4bff94905e6926ad8946456a96eafd0d9
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-07 22:06:42 +0300

    Add MultiplicationTable

---
+++ Basic syntax conditional statements and loops/LAB/10. Multiplication Table/MultiplicationTable.cs
@@ -0,0 +1,17 @@
+﻿namespace _10._Multiplication_Table
+{
+    using System;
+
+    public class MultiplicationTable
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} X {i} = {number * i}");
+            }
+        }
+    }
+}
