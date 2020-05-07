d4216f0b5bc30da95e94507886bad9b8e965f767
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-07 22:10:05 +0300

    Add Basic syntax conditional statements and loops LAB.csproj

---
+++ Basic syntax conditional statements and loops/LAB/Basic syntax conditional statements and loops LAB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_syntax_conditional_statements_and_loops_LAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
