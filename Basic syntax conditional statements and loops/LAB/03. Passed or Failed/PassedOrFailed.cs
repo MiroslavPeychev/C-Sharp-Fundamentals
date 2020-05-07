ccc9e1836667c809159e76e0da3ee19aa343980c
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-07 21:57:37 +0300

    Add PassedOrFailed

---
+++ Basic syntax conditional statements and loops/LAB/03. Passed or Failed/PassedOrFailed.cs
@@ -0,0 +1,24 @@
+﻿namespace _03._Passed_or_Failed
+{
+    using System;
+
+    public class PassedOrFailed
+    {
+        public static void Main()
+        {
+            double studentGrade = double.Parse(Console.ReadLine());
+            string passOrFaild = string.Empty;
+
+            if (studentGrade >= 3.00)
+            {
+                passOrFaild = "Passed!";
+            }
+            else
+            {
+                passOrFaild = "Failed!";
+            }
+
+            Console.WriteLine(passOrFaild);
+        }
+    }
+}
