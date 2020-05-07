322fc1fbcd8ea1e5dce6ffe4be03157853c6c27e
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-07 22:07:31 +0300

    Add MultiplicationTable2

---
+++ Basic syntax conditional statements and loops/LAB/11. Multiplication Table 2.0/MultiplicationTable2.cs
@@ -0,0 +1,24 @@
+﻿namespace _11._Multiplication_Table_2._0
+{
+    using System;
+
+    public class MultiplicationTable2
+    {
+        public static void Main()
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            int multiplierCount = int.Parse(Console.ReadLine());
+
+            if (multiplierCount > 10)
+            {
+                Console.WriteLine($"{inputNumber} X {multiplierCount} = {inputNumber * multiplierCount}");
+                return;
+            }
+
+            for (int i = multiplierCount; i <= 10; i++)
+            {
+                Console.WriteLine($"{inputNumber} X {i} = {inputNumber * i}");
+            }
+        }
+    }
+}
