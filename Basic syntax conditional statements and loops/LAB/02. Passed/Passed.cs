b20fcdc11baf1ab09fbe16bc0ab6ace05dddca63
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-07 21:56:45 +0300

    Add Passed

---
+++ Basic syntax conditional statements and loops/LAB/02. Passed/Passed.cs
@@ -0,0 +1,17 @@
+﻿namespace _02._Passed
+{
+    using System;
+
+    public class Passed
+    {
+        public static void Main()
+        {
+            float studentGrade = float.Parse(Console.ReadLine());
+
+            if (studentGrade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+        }
+    }
+}
