b4dc0568535ce8d4881848ca2c55e38a8070f7e2
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-07 22:05:24 +0300

    Add SumOfOddNumbers

---
+++ Basic syntax conditional statements and loops/LAB/09. Sum of Odd Numbers/SumOfOddNumbers.cs
@@ -0,0 +1,22 @@
+﻿namespace _09._Sum_of_Odd_Numbers
+{
+    using System;
+
+    public class SumOfOddNumbers
+    {
+        public static void Main()
+        {
+            int oddNumbersCount = int.Parse(Console.ReadLine());
+            int sumOfOddNumbers = 0;
+            int startingNumber = 1;
+
+            for (int i = startingNumber; i < oddNumbersCount * 2; i += 2)
+            {
+                Console.WriteLine(i);
+                sumOfOddNumbers += i;
+            }
+
+            Console.WriteLine($"Sum: {sumOfOddNumbers}");
+        }
+    }
+}
