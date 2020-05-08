3badf129cda166679e2e2fcdb6838c65df38bef9
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-08 21:54:13 +0300

    Add Basic syntax conditional statements and loops.csproj

---
+++ Basic syntax conditional statements and loops/Exercise/Basic syntax conditional statements and loops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_syntax_conditional_statements_and_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
