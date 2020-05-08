729b2b9c94d3bd7892ce4316320e647a9c4bf70e
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-08 21:52:17 +0300

    Add Triangle of numbers

---
+++ Basic syntax conditional statements and loops/Exercise/08. Triangle of Numbers/TriangleOfNumbers.cs
@@ -0,0 +1,22 @@
+﻿namespace _08._Triangle_of_Numbers
+{
+    using System;
+
+    public class TriangleOfNumbers
+    {
+        public static void Main()
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= inputNumber; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write($"{row} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
