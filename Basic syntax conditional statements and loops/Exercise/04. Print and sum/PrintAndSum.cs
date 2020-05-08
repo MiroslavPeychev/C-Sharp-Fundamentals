075b6e4a9084ab1ec4dec07739d7709e261564d3
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-08 21:49:49 +0300

    Add Print And Sum

---
+++ Basic syntax conditional statements and loops/Exercise/04. Print and sum/PrintAndSum.cs
@@ -0,0 +1,25 @@
+﻿namespace _04._Print_and_sum
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class PrintAndSum
+    {
+        public static void Main()
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+            List<int> numbers = new List<int>();
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                sum += i;
+                numbers.Add(i);
+            }
+
+            Console.WriteLine(String.Join(" ", numbers));
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
