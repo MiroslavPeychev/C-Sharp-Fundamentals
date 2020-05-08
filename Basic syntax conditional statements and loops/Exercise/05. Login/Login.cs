2a4c253e155a076b1606029f8ada7b935ceba08c
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-08 21:50:18 +0300

    Add Login

---
+++ Basic syntax conditional statements and loops/Exercise/05. Login/Login.cs
@@ -0,0 +1,46 @@
+﻿namespace _05._Login
+{
+    using System;
+
+    public class Login
+    {
+        public static void Main()
+        {
+            string userName = Console.ReadLine();
+            string passWord = string.Empty;
+
+
+            for (int i = userName.Length - 1; i >= 0; i--)
+            {
+                passWord += userName[i];
+            }
+
+            int incorrectPasswordCounter = 0;
+
+            bool isLoggedIn = false;
+
+            while (incorrectPasswordCounter < 4)
+            {
+                string inputPassword = Console.ReadLine();
+
+                if (passWord == inputPassword)
+                {
+                    isLoggedIn = true;
+                    Console.WriteLine($"User {userName} logged in.");
+                    break;
+                }
+                else if (incorrectPasswordCounter != 3)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+
+                incorrectPasswordCounter++;
+            }
+
+            if (!isLoggedIn)
+            {
+                Console.WriteLine($"User {userName} blocked!");
+            }
+        }
+    }
+}
