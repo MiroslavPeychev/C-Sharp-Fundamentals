8302428ac82f3f9f8785f3ce2b5c75baadbb931f
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-08 21:48:12 +0300

    Add Ages

---
+++ Basic syntax conditional statements and loops/Exercise/01. Ages/Ages.cs
@@ -0,0 +1,36 @@
+﻿namespace _01._Ages
+{
+    using System;
+
+    public class Ages
+    {
+        public static void Main()
+        {
+            int ages = int.Parse(Console.ReadLine());
+            string result = string.Empty;
+
+            if (ages >= 0 && ages <= 2)
+            {
+                result = "baby";
+            }
+            else if (ages >= 3 && ages <= 13)
+            {
+                result = "child";
+            }
+            else if (ages >= 14 && ages <= 19)
+            {
+                result = "teenager";
+            }
+            else if (ages >= 20 && ages <= 65)
+            {
+                result = "adult";
+            }
+            else if (ages >= 65)
+            {
+                result = "elder";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
