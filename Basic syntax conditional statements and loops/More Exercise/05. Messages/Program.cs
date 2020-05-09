9d3a24333757c22eec51b21a9c57b1bb0c560922
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-09 23:48:31 +0300

    Add Messages

---
+++ Basic syntax conditional statements and loops/More Exercise/05. Messages/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _05._Messages
+{
+    using System;
+
+    public class Messages
+    {
+        public static void Main()
+        {
+            int clicks = int.Parse(Console.ReadLine());
+            string typedMessage = string.Empty;
+
+            for (int i = 0; i < clicks; i++)
+            {
+                string digits = Console.ReadLine();
+                int digitLength = digits.Length;
+                int digit = digits[0] - '0';
+                int offset = (digit - 2) * 3;
+
+                if (digit == 0)
+                {
+                    typedMessage += (char)(digit + 32);
+                    continue;
+                }
+
+                if (digit == 8 || digit == 9)
+                {
+                    offset++;
+                }
+
+                int numberInAscciTable = offset + digitLength - 1;
+                typedMessage += (char)(numberInAscciTable + 97);
+            }
+
+            Console.WriteLine(typedMessage);
+        }
+    }
+}
