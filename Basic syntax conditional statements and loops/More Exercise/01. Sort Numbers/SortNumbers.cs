359ee83bdbde8d7e826f4d20b211cb6efc4feef0
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-09 21:44:39 +0300

    Add Sort numbers

---
+++ Basic syntax conditional statements and loops/More Exercise/01. Sort Numbers/SortNumbers.cs
@@ -0,0 +1,28 @@
+﻿namespace _01._Sort_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SortNumbers
+    {
+        public static void Main()
+        {
+            int number = 0;
+            List<int> listOfnumbers = new List<int>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+                listOfnumbers.Add(number);
+            }
+
+            var sortedNumbers = listOfnumbers.OrderByDescending(l => l);
+
+            foreach (var num in sortedNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
