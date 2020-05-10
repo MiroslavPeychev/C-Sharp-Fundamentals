0b4fe3f5ebb3dda61cefe7c436246685fc240468
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-10 22:14:33 +0300

    Add Condense array to number

---
+++ Arrays/LAB/08. Condense Array to Number/CondenseArrayToNumber.cs
@@ -0,0 +1,39 @@
+﻿namespace _08._Condense_Array_to_Number
+{
+    using System;
+    using System.Linq;
+
+    public class CondenseArrayToNumber
+    {
+        public static void Main()
+        {
+            int[] originalArray = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] condensedArray = new int[originalArray.Length - 1];
+
+            if (originalArray.Length == 1)
+            {
+                Console.WriteLine(originalArray[0]);
+                return;
+            }
+
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                for (int j = 0; j < condensedArray.Length - i; j++)
+                {
+                    condensedArray[j] = originalArray[j] + originalArray[j + 1];
+                }
+
+                originalArray = condensedArray;
+            }
+
+            int condensedNumber = condensedArray[0];
+
+            Console.WriteLine(condensedNumber);
+        }
+    }
+}
