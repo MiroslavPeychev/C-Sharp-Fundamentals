eda711983b5cba91eedbe9ec87fcc85162204154
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-10 22:10:14 +0300

    Add Reverse array of strings

---
+++ Arrays/LAB/04. Reverse Array of Strings/ReverseArrayOfStrings.cs
@@ -0,0 +1,16 @@
+﻿namespace _04._Reverse_Array_of_Strings
+{
+    using System;
+    using System.Linq;
+
+    public class ReverseArrayOfStrings
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            string[] reversed = input.Reverse().ToArray();
+
+            Console.WriteLine(string.Join(" ", reversed));
+        }
+    }
+}
