f7d6d14e26a7aaf55dd61012e40d73b2f719223c
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-10 21:52:17 +0300

    Add Day of Week

---
+++ Arrays/LAB/01. Day of Week/DayOfWeek.cs
@@ -0,0 +1,34 @@
+﻿namespace _01._Day_of_Week
+{
+    using System;
+
+    public class DayOfWeek
+    {
+        public static void Main()
+        {
+            string[] days = {"Monday",
+                          "Tuesday",
+                          "Wednesday",
+                          "Thursday",
+                          "Friday",
+                          "Saturday",
+                          "Sunday"};
+
+            //Console.WriteLine("Please, insert an integer number between 1 and 7");
+            int day = int.Parse(Console.ReadLine());
+
+            string result = string.Empty;
+
+            if (day<1||day>7)
+            {
+                result = "Invalid day!";
+            }
+            else
+            {
+                result = $"{days[day - 1]}"; //The first day in the array is on index 0, not 1
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
