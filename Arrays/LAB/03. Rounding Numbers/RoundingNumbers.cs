9b3a9ac29486419e090347212b458f2fa3348bc7
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-10 22:09:19 +0300

    Rounding numbers

---
+++ Arrays/LAB/03. Rounding Numbers/RoundingNumbers.cs
@@ -0,0 +1,24 @@
+﻿namespace _03._Rounding_Numbers
+{
+    using System;
+    using System.Linq;
+
+    public class RoundingNumbers
+    {
+        public static void Main()
+        {
+            double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                double number = numbers[i];
+                double roudedNumber = Math.Round(number, MidpointRounding.AwayFromZero);
+
+                Console.WriteLine($"{Convert.ToDecimal(number)} => {Convert.ToDecimal(roudedNumber)}");
+            }
+        }
+    }
+}
