02acc1db27c75bd818f6b97416cb16d4d51b323c
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-10 22:13:43 +0300

    Add Equal arrays

---
+++ Arrays/LAB/07. Equal Arrays/EqualArrays.cs
@@ -0,0 +1,45 @@
+﻿namespace _07._Equal_Arrays
+{
+    using System;
+    using System.Linq;
+
+    public class EqualArrays
+    {
+        public static void Main()
+        {
+            int[] firstArray = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] secondArray = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstArraySum = 0;
+            bool areIdentical = true;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+
+                if (firstArray[i] != secondArray[i])
+                {
+                    areIdentical = false;
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+                else
+                {
+                    firstArraySum += firstArray[i];
+                }
+            }
+
+            if (areIdentical)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {firstArraySum}");
+            }
+        }
+    }
+}
