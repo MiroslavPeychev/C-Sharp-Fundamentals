db552d7a2660a60d77569cd8cca8981a5dff0576
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-10 22:13:05 +0300

    Add Even and odd subtraction

---
+++ Arrays/LAB/06. Even and Odd Subtraction/EvenAndOddSubtraction.cs
@@ -0,0 +1,38 @@
+﻿namespace _06._Even_and_Odd_Subtraction
+{
+    using System;
+    using System.Linq;
+
+    public class EvenAndOddSubtraction
+    {
+        public static void Main()
+        {
+            int[] numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int evenNumbersSum = 0;
+            int oddNumbersSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+
+                if (currentNumber % 2 == 0)
+                {
+                    evenNumbersSum += currentNumber;
+                }
+                else
+                {
+                    oddNumbersSum += currentNumber;
+                }
+            }
+
+            int difference = evenNumbersSum - oddNumbersSum;
+
+            Console.WriteLine(difference);
+        }
+    }
+}
