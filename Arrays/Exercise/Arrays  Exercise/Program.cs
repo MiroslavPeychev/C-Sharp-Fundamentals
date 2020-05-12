1a546d8873e5221e79c6520e88b4aa2de152c273
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-12 22:52:34 +0300

    Add Arrays Exercise.csproj

---
+++ Arrays/Exercise/Arrays  Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arrays__Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
