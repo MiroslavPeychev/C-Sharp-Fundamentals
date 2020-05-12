35eea4992ceac09372c0eb2393970b588d854723
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-12 22:28:08 +0300

    Add Equal sum

---
+++ Arrays/Exercise/06. Equal Sum/EqualSum.cs
@@ -0,0 +1,45 @@
+﻿namespace _06._Equal_Sum
+{
+    using System;
+    using System.Linq;
+
+    public class EqualSum
+    {
+        public static void Main()
+        {
+            int[] numbers = Console
+                    .ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+            int leftSum = 0;
+            int rightSum = numbers.Sum();
+            bool areEqual = false;
+            int indexOfequality = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                rightSum -= numbers[i];
+
+                if (leftSum == rightSum)
+                {
+                    indexOfequality = i;
+                    areEqual = true;
+                    break;
+                }
+
+                leftSum += numbers[i];
+            }
+
+            if (!areEqual)
+            {
+                Console.WriteLine("no");
+            }
+            else
+            {
+                Console.WriteLine(indexOfequality);
+            }
+        }
+    }
+}
