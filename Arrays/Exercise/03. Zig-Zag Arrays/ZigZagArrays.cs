5e3db4cf983285fcf45a9131c54ab6b190ea151d
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-12 22:26:25 +0300

    Add Zig-Zag arrays

---
+++ Arrays/Exercise/03. Zig-Zag Arrays/ZigZagArrays.cs
@@ -0,0 +1,39 @@
+﻿namespace _03._Zig_Zag_Arrays
+{
+    using System;
+    using System.Linq;
+
+    public class ZigZagArrays
+    {
+        public static void Main()
+        {
+            int inputLinesCount = int.Parse(Console.ReadLine());
+
+            int[] firstArray = new int[inputLinesCount];
+            int[] secondArray = new int[inputLinesCount];
+
+            for (int i = 0; i < inputLinesCount; i++)
+            {
+                int[] numbers = Console
+                    .ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (i%2==0)
+                {
+                    firstArray[i] = numbers[0];
+                    secondArray[i] = numbers[1];
+                }
+                else
+                {
+                    firstArray[i] = numbers[1];
+                    secondArray[i] = numbers[0];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", firstArray));
+            Console.WriteLine(string.Join(" ", secondArray));
+        }
+    }
+}
