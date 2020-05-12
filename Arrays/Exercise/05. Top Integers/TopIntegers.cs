5073227c2edc9bad99181b46625d28a9bcbb2a3c
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-12 22:27:42 +0300

    Add Top integers

---
+++ Arrays/Exercise/05. Top Integers/TopIntegers.cs
@@ -0,0 +1,44 @@
+﻿namespace _05._Top_Integers
+{
+    using System;
+    using System.Linq;
+
+    public class TopIntegers
+    {
+        public static void Main()
+        {
+            int[] numbers = Console
+                    .ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+            int lastNumber = numbers[numbers.Length - 1];//Last number is always topInteger
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int currentNumber = numbers[i];
+
+                bool isTopInteger = true;
+
+                for (int j = i + 1; j <= numbers.Length-1; j++)
+                {
+                    int numberToCompare = numbers[j];
+
+                    if (currentNumber <= numberToCompare)
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+
+                if (isTopInteger)
+                {
+                    Console.Write(currentNumber + " ");
+                }
+            }
+
+            Console.WriteLine(lastNumber);
+        }
+    }
+}
