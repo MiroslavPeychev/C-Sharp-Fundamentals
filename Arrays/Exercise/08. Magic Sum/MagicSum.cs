272e09d9371b44a935f87d52b3b98e8e45cf7183
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-12 22:30:11 +0300

    Add Magic sum

---
+++ Arrays/Exercise/08. Magic Sum/MagicSum.cs
@@ -0,0 +1,34 @@
+﻿namespace _08._Magic_Sum
+{
+    using System;
+    using System.Linq;
+
+    public class MagicSum
+    {
+        public static void Main()
+        {
+            int[] numbers = Console
+                    .ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int firstNumber = numbers[i];
+
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    int secondNumber = numbers[j];
+
+                    if (firstNumber+secondNumber==magicNumber)
+                    {
+                        Console.WriteLine($"{firstNumber} {secondNumber}");
+                    }
+                }
+            }
+        }
+    }
+}
