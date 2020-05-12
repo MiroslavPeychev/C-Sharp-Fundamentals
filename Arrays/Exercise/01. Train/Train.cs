cc5d5f5c0a277fed78c8d0afd1a23e81414cdf45
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-12 22:25:13 +0300

    Add Train

---
+++ Arrays/Exercise/01. Train/Train.cs
@@ -0,0 +1,27 @@
+﻿namespace _01._Train
+{
+    using System;
+    using System.Linq;
+
+    public class Train
+    {
+        public static void Main()
+        {
+            int numberOfWagons = int.Parse(Console.ReadLine());
+
+            int[] passangers = new int[numberOfWagons];
+
+            for (int i = 0; i < numberOfWagons; i++)
+            {
+                int currentWagonPassangers = int.Parse(Console.ReadLine());
+
+                passangers[i] = currentWagonPassangers;
+            }
+
+            int allPassangersCount = passangers.Sum();
+
+            Console.WriteLine(string.Join(" ", passangers));
+            Console.WriteLine(allPassangersCount);
+        }
+    }
+}
