de111037d05af4f6df718717ca4d044ba1cfd5d2
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-12 22:25:46 +0300

    Add Common elements

---
+++ Arrays/Exercise/02. Common Elements/CommonElements.cs
@@ -0,0 +1,34 @@
+﻿namespace _02._Common_Elements
+{
+    using System;
+
+    public class CommonElements
+    {
+        public static void Main()
+        {
+            string[] firstArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] secondArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                string currentSecondArrElement = secondArray[i];
+
+                for (int j = 0; j < firstArray.Length; j++)//if (firstArray.Contains(currentSecondArrElement)=>usint System.Linq;
+                {
+                    string currentFirstArrElement = firstArray[j];
+
+                    if (currentSecondArrElement==currentFirstArrElement)
+                    {
+                        Console.Write(currentSecondArrElement + " ");
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
