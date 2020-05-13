31cfaaf451671a52ce2a3d0964e08f98ee783e4f
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-13 21:21:18 +0300

    Add Pascal triangle

---
+++ Arrays/More Exercise/02. Pascal Triangle/PascalTriangle.cs
@@ -0,0 +1,15 @@
+﻿namespace _02._Pascal_Triangle
+{
+    using System;
+
+    public class PascalTriangle
+    {
+        public static void Main()
+        {
+            
+        }
+
+        
+        }
+    }
+}
