0a30c5095e4f856e321a36bb16b87ce8294c72ce
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-14 22:31:53 +0300

    Add Chars to string

---
+++ Data Types and Variables/LAB/09. Chars to String/CharsToString.cs
@@ -0,0 +1,19 @@
+﻿namespace _09._Chars_to_String
+{
+    using System;
+
+    public class CharsToString
+    {
+        public static void Main()
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < 3; i++)
+            {
+                result += Console.ReadLine();
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
