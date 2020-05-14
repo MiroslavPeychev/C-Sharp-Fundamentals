6e726c9d337bf4d59a139414f4dc5a593e661fe4
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-14 22:28:14 +0300

    Add Pound to dollars

---
+++ Data Types and Variables/LAB/02. Pounds to Dollars/PoundsToDollars.cs
@@ -0,0 +1,17 @@
+﻿namespace _02._Pounds_to_Dollars
+{
+    using System;
+
+    public class PoundsToDollars
+    {
+        public static void Main()
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+            //1 British Pound = 1.31 Dollars
+
+            decimal dollars = pounds * 1.31m;
+
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
