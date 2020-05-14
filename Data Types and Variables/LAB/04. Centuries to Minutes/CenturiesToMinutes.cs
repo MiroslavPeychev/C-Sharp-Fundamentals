2d727f5d2876ad2f9fb3af2b3de30686863bb1e3
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-14 22:29:28 +0300

    Add Centuries to minutes

---
+++ Data Types and Variables/LAB/04. Centuries to Minutes/CenturiesToMinutes.cs
@@ -0,0 +1,21 @@
+﻿namespace _04._Centuries_to_Minutes
+{
+    using System;
+
+    public class CenturiesToMinutes
+    {
+        public static void Main()
+        {
+            int centuries = int.Parse(Console.ReadLine());
+
+            int years = centuries * 100;
+
+            //•	Assume that a year has 365.2422 days at average (the Tropical year).
+            int days = (int)(years * 365.2422);
+            int hours = days * 24;
+            int minutes = hours * 60;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
