75965c3152e4f25ca9defa16a6ab039265e943e5
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-14 22:30:52 +0300

    Add Concat names

---
+++ Data Types and Variables/LAB/07. Concat Names/ConcatNames.cs
@@ -0,0 +1,16 @@
+﻿namespace _07._Concat_Names
+{
+    using System;
+
+    public class ConcatNames
+    {
+        public static void Main()
+        {
+            string firstName = Console.ReadLine();
+            string secondName = Console.ReadLine();
+            string delimeter = Console.ReadLine();
+
+            Console.WriteLine($"{firstName}{delimeter}{secondName}");
+        }
+    }
+}
