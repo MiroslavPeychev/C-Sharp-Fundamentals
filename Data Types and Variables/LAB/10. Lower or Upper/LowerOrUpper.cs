7b6c48e0c8992f7816a6f5d8dd910cb676e44069
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-14 22:32:33 +0300

    Add Lower or Upper

---
+++ Data Types and Variables/LAB/10. Lower or Upper/LowerOrUpper.cs
@@ -0,0 +1,22 @@
+﻿namespace _10._Lower_or_Upper
+{
+    using System;
+
+    public class LowerOrUpper
+    {
+        public static void Main()
+        {
+            char input = char.Parse(Console.ReadLine());
+            bool isUpper = char.IsUpper(input);
+
+            if (isUpper)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
