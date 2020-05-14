a55096dc1fe1c381230d8e29c36b88f909450a4f
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-14 22:33:19 +0300

    Add Refactor Volume of pyramid

---
+++ Data Types and Variables/LAB/11. Refactor Volume of Pyramid/RefactorVolumeOfPyramid.cs
@@ -0,0 +1,23 @@
+﻿namespace _11._Refactor_Volume_of_Pyramid
+{
+    using System;
+
+    public class RefactorVolumeOfPyramid
+    {
+        public static void Main()
+        {
+
+
+            Console.Write("Length: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            double heigth = double.Parse(Console.ReadLine());
+
+            double pyramidVolume = (length * width * heigth) / 3;
+
+            Console.WriteLine($"Pyramid Volume: {pyramidVolume:f2}");
+        }
+    }
+}
