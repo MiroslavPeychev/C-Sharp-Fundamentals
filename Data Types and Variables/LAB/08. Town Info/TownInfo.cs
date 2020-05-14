48347b736e6fcffaaf9b3e63d8962ff432fc7b3d
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-14 22:31:28 +0300

    Add Town info

---
+++ Data Types and Variables/LAB/08. Town Info/TownInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace _08._Town_Info
+{
+    using System;
+
+    public class TownInfo
+    {
+        public static void Main()
+        {
+            string townName = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            double area = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {townName} has population of {population} and area {area} square km.");
+        }
+    }
+}
