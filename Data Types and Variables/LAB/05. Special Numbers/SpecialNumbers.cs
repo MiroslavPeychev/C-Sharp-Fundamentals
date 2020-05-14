f50fcb11c656d1f7a3945da0974a05d414c387b9
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-14 22:29:59 +0300

    Add Special numbers

---
+++ Data Types and Variables/LAB/05. Special Numbers/SpecialNumbers.cs
@@ -0,0 +1,30 @@
+﻿namespace _05._Special_Numbers
+{
+    using System;
+
+    public class SpecialNumbers
+    {
+        public static void Main()
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= inputNumber; i++)
+            {
+                int number = i;
+                int sumDigit = 0;
+
+                while (number != 0)
+                {
+                    int lastDigit = number % 10;
+                    number /= 10;
+
+                    sumDigit += lastDigit;
+                }
+
+                var isSpecial = sumDigit == 5 || sumDigit == 7 || sumDigit == 11;
+
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
