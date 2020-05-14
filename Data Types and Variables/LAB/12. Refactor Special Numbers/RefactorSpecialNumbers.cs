6ded769315de5d868d7c16dc12960aeeb15426c8
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-14 22:33:54 +0300

    Add Refactor special numbers

---
+++ Data Types and Variables/LAB/12. Refactor Special Numbers/RefactorSpecialNumbers.cs
@@ -0,0 +1,28 @@
+﻿namespace _12._Refactor_Special_Numbers
+{
+    using System;
+
+    public class RefactorSpecialNumbers
+    {
+        public static void Main()
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= inputNumber; i++)
+            {
+                int number = i;
+                int sumDigit = 0;
+
+                while (number != 0)
+                {
+                    sumDigit += number % 10;
+                    number /= 10;
+                }
+
+                var isSpecial = sumDigit == 5 || sumDigit == 7 || sumDigit == 11;
+
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
