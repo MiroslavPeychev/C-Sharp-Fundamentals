403a51361348cfb768b57534b337aa9dcea21dc4
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-14 22:28:52 +0300

    Add Exact sum of real numbers

---
+++ Data Types and Variables/LAB/03. Exact Sum of Real Numbers/ExactSumOfRealNumbers.cs
@@ -0,0 +1,21 @@
+﻿namespace _03._Exact_Sum_of_Real_Numbers
+{
+    using System;
+
+    public class ExactSumOfRealNumbers
+    {
+        public static void Main()
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            for (int i = 0; i < inputNumber; i++)
+            {
+                decimal numberToSum = decimal.Parse(Console.ReadLine());
+                sum += numberToSum;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
