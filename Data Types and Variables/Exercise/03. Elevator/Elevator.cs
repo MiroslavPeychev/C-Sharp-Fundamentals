7bbe9129411d95cb28b0692ed8349394d9820196
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-15 21:04:32 +0300

    Add Elevator

---
+++ Data Types and Variables/Exercise/03. Elevator/Elevator.cs
@@ -0,0 +1,28 @@
+﻿namespace _03._Elevator
+{
+    using System;
+
+    public class Elevator
+    {
+        public static void Main()
+        {
+            int passangers = int.Parse(Console.ReadLine());
+            int elevatorCapacity = int.Parse(Console.ReadLine());
+
+            int courses = passangers / elevatorCapacity;
+            bool hasAnExtraCourse = passangers % elevatorCapacity != 0;
+
+            if (hasAnExtraCourse)
+            {
+                courses++;
+            }
+
+            Console.WriteLine(courses);
+
+            //OR
+            //int courses = (int)Math.Ceiling((double)passangers/elevatorCapacity);
+            //Console.WriteLine(courses);
+
+        }
+    }
+}
