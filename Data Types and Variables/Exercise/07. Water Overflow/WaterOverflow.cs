33361d54b37b636aa6bf64f7414e0a5ebb10ba8a
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-15 21:07:06 +0300

    Add Water Overflow

---
+++ Data Types and Variables/Exercise/07. Water Overflow/WaterOverflow.cs
@@ -0,0 +1,30 @@
+﻿namespace _07._Water_Overflow
+{
+    using System;
+
+    public class WaterOverflow
+    {
+        public static void Main()
+        {
+            int inputLines = int.Parse(Console.ReadLine());
+            int tankCapacity = 255;
+            int totalLitersInTank = 0;
+
+            for (int i = 0; i < inputLines; i++)
+            {
+                int waterToAdd = int.Parse(Console.ReadLine());
+
+                if (totalLitersInTank + waterToAdd <= tankCapacity)
+                {
+                    totalLitersInTank += waterToAdd;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+            }
+            Console.WriteLine(totalLitersInTank);
+        }
+    }
+}
