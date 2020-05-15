341a916b0f0d7b1dd4de74f9ed4ed873cae48f2a
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-15 21:04:58 +0300

    Add Sum of chars

---
+++ Data Types and Variables/Exercise/04. Sum of Chars/SumOfChars.cs
@@ -0,0 +1,24 @@
+﻿namespace _04._Sum_of_Chars
+{
+    using System;
+
+    public class SumOfChars
+    {
+        public static void Main()
+        {
+            int numberOfImputlines = int.Parse(Console.ReadLine());
+
+            int totalSum = 0;
+
+            for (int i = 0; i < numberOfImputlines; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+
+                totalSum += letter;
+
+            }
+
+            Console.WriteLine($"The sum equals: {totalSum}");
+        }
+    }
+}
