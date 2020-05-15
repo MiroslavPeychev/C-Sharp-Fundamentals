f3f615f778bbc832148fa36de5e2511d69749f45
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-15 21:05:29 +0300

    Add Print part of ASCII table

---
+++ Data Types and Variables/Exercise/05. Print Part Of ASCII Table/PrintPartOfASCIITable.cs
@@ -0,0 +1,27 @@
+﻿namespace _05._Print_Part_Of_ASCII_Table
+{
+    using System;
+
+    public class PrintPartOfASCIITable
+    {
+        public static void Main()
+        {
+            int startAsciiCode = int.Parse(Console.ReadLine());
+            int endAsciiCode = int.Parse(Console.ReadLine());
+
+            for (int i = startAsciiCode; i <= endAsciiCode; i++)
+            {
+                int currentCharacter = i;
+                Console.Write($"{(char)(currentCharacter)} ");
+            }
+
+            Console.WriteLine();
+
+            //OR
+            //for (char i = (char)startAsciiCode; i <= endAsciiCode; i++)
+            //{
+            //    Console.Write(i+" ");
+            //}
+        }
+    }
+}
