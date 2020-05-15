53ffeddeb4d6d3595736fb641e255bfc4d9fd0bf
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-15 21:07:58 +0300

    Add Spice must flow

---
+++ Data Types and Variables/Exercise/09. Spice Must Flow/SpiceMustFlow.cs
@@ -0,0 +1,33 @@
+﻿namespace _09._Spice_Must_Flow
+{
+    using System;
+
+    public class SpiceMustFlow
+    {
+        public static void Main()
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int spiceMined = 0;
+
+            int dropDownYieldConstant = 10;
+            int workersConsume = 26;
+            int days = 0;
+
+            while (startingYield >= 100)
+            {
+                spiceMined += startingYield;
+                spiceMined -= workersConsume;
+                startingYield -= dropDownYieldConstant;
+                days++;
+            }
+
+            if (spiceMined != 0)
+            {
+                spiceMined -= workersConsume;
+            }
+
+            Console.WriteLine(days);
+            Console.WriteLine(spiceMined);
+        }
+    }
+}
