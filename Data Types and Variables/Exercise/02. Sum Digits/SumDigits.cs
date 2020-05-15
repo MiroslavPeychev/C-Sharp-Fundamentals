bacd66440e4c66ff5ea43ff67bda3f852097e269
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-15 21:04:01 +0300

    Add Sum digits

---
+++ Data Types and Variables/Exercise/02. Sum Digits/SumDigits.cs
@@ -0,0 +1,36 @@
+﻿namespace _02._Sum_Digits
+{
+    using System;
+
+    public class SumDigits
+    {
+        public static void Main()
+        {
+            string inputNumberAsString = Console.ReadLine();
+            int sumOfDigits = 0;
+
+            for (int i = 0; i < inputNumberAsString.Length; i++)
+            {
+                int lastDigit = int.Parse(inputNumberAsString[i].ToString());
+
+                sumOfDigits += lastDigit;
+            }
+
+            Console.WriteLine(sumOfDigits);
+
+            //OR
+
+            //int number = int.Parse(Console.ReadLine());
+            //int sumDigits = 0;
+
+            //number = Math.Abs(number);
+
+            //while (number != 0)
+            //{
+            //    sumDigits += number % 10;
+            //    number /= 10;
+            //}
+            //Console.WriteLine($"{sumDigits}");
+        }
+    }
+}
