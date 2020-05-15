558230184210cc055a7c11048aeab84954ca737e
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-15 21:08:26 +0300

    Add Poke mon

---
+++ Data Types and Variables/Exercise/10. Poke Mon/PokeMon.cs
@@ -0,0 +1,31 @@
+﻿namespace _10._Poke_Mon
+{
+    using System;
+
+    public class PokeMon
+    {
+        public static void Main()
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int pokedTargets = 0;
+            int originalPower = pokePower;
+
+            while (pokePower >= distance)
+            {
+                pokePower -= distance;
+                pokedTargets++;
+
+                if (pokePower == originalPower / 2 && exhaustionFactor != 0)
+                {
+                    pokePower /= exhaustionFactor;
+                }
+            }
+
+            Console.WriteLine(pokePower);
+            Console.WriteLine(pokedTargets);
+        }
+    }
+}
