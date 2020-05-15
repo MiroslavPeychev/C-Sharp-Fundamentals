658ad893bb3ede7c58eef97520f05397606d95d3
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-15 21:06:39 +0300

    Add Triplets of Latin Letters

---
+++ Data Types and Variables/Exercise/06. Triples of Latin Letters/TriplesOfLatinLetters.cs
@@ -0,0 +1,38 @@
+﻿namespace _06._Triples_of_Latin_Letters
+{
+    using System;
+
+    public class TriplesOfLatinLetters
+    {
+        public static void Main()
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            for (char first = 'a'; first < 'a' + inputNumber; first++)
+            {
+                for (char second = 'a'; second < 'a' + inputNumber; second++)
+                {
+                    for (char third = 'a'; third < 'a' + inputNumber; third++)
+                    {
+
+                        Console.WriteLine($"{first}{second}{third}");
+                    }
+                }
+            }
+
+            //int n = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        for (int q = 0; q < n; q++)
+            //        {
+
+            //            Console.WriteLine($"{(char)('a' + i)}{(char)('a' + j)}{(char)('a' + q)}");
+            //        }
+            //    }
+            //}
+        }
+    }
+}
