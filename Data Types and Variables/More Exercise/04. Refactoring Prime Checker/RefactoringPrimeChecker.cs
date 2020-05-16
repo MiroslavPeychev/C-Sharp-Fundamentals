ce0650b8ba9168f076044846fdadef0e63836a0b
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-16 23:21:22 +0300

    Add Refactoring prime checker

---
+++ Data Types and Variables/More Exercise/04. Refactoring Prime Checker/RefactoringPrimeChecker.cs
@@ -0,0 +1,28 @@
+﻿namespace _04._Refactoring_Prime_Checker
+{
+    using System;
+
+    public class RefactoringPrimeChecker
+    {
+        public static void Main()
+        {
+            int endNumber = int.Parse(Console.ReadLine());
+
+            for (int number = 2; number <= endNumber; number++)
+            {
+                bool isPrime = true;
+
+                for (int divider = 2; divider < number; divider++)
+                {
+                    if (number % divider == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                Console.WriteLine($"{number} -> {isPrime.ToString().ToLower()}");
+            }
+        }
+    }
+}
