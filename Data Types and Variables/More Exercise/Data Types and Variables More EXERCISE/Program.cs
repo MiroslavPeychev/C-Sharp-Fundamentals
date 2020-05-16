debbb6bea30fc7d22e7e4ef9f53c6a138df5290f
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-16 23:23:12 +0300

    Add Data Types and Variables More EXERCISE.csproj

---
+++ Data Types and Variables/More Exercise/Data Types and Variables More EXERCISE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Data_Types_and_Variables_More_EXERCISE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
