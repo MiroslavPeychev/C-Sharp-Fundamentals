8060e0818047dd6b331ab6fa580c8f63fd44413b
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-16 23:22:15 +0300

    Add Decrypting message

---
+++ Data Types and Variables/More Exercise/05. Decrypting Message/DecryptingMessage.cs
@@ -0,0 +1,25 @@
+﻿namespace _05._Decrypting_Message
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class DecryptingMessage
+    {
+        public static void Main()
+        {
+            int decryptingKey = int.Parse(Console.ReadLine());
+            int numberOfInputLines = int.Parse(Console.ReadLine());
+
+            List<char> decryptedMessage = new List<char>();
+
+            for (int i = 0; i < numberOfInputLines; i++)
+            {
+                char currentChar = char.Parse(Console.ReadLine());
+                char newChar = (char)(decryptingKey + currentChar);
+                decryptedMessage.Add(newChar);
+            }
+
+            Console.WriteLine(String.Join("", decryptedMessage));
+        }
+    }
+}
