96deb6821914c116fe4be203b5a407d5cf8dcfe4
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-16 23:22:46 +0300

    Add Balanced brackets

---
+++ Data Types and Variables/More Exercise/06. Balanced Brackets/BalancedBrackets.cs
@@ -0,0 +1,49 @@
+﻿namespace _06._Balanced_Brackets
+{
+    using System;
+
+    public class BalancedBrackets
+    {
+        public static void Main()
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            int openingBracketCounter = 0;
+            int closingBrackrtCounter = 0;
+
+            for (int i = 1; i <= numberOfLines; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "(")
+                {
+                    openingBracketCounter++;
+                }
+                else if (input == ")")
+                {
+                    closingBrackrtCounter++;
+
+                    if (openingBracketCounter - closingBrackrtCounter != 0)
+                    {
+                        Console.WriteLine("UNBALANCED");
+                        return;
+                    }
+                }
+            }
+
+            string result = string.Empty;
+            bool areBalanced = (openingBracketCounter == closingBrackrtCounter);
+
+            if (areBalanced)
+            {
+                result = "BALANCED";
+            }
+            else
+            {
+                result = "UNBALANCED";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
