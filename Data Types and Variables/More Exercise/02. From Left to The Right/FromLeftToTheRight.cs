f30a5604d2709c6dfa34294160a13ece9336f55a
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-16 23:20:10 +0300

    Add From left to the right

---
+++ Data Types and Variables/More Exercise/02. From Left to The Right/FromLeftToTheRight.cs
@@ -0,0 +1,50 @@
+﻿namespace _02._From_Left_to_The_Right
+{
+    using System;
+    using System.Numerics;
+
+    public class FromLeftToTheRight
+    {
+        public static void Main()
+        {
+            int numberOfInputLines = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numberOfInputLines; i++)
+            {
+
+                string input = Console.ReadLine();
+                string[] tokens = input.Split(" ");
+                long firstNumber = long.Parse(tokens[0]);
+                long secondNumber = long.Parse(tokens[1]);
+
+                long sumDigits = 0;
+
+
+                if (firstNumber > secondNumber)
+                {
+                    long currentNumber = firstNumber;
+
+                    while (currentNumber != 0)
+                    {
+                        sumDigits += currentNumber % 10;
+                        currentNumber /= 10;
+                    }
+
+                    Console.WriteLine($"{Math.Abs(sumDigits)}");
+                }
+                else
+                {
+                    long currentNumber = secondNumber;
+
+                    while (currentNumber != 0)
+                    {
+                        sumDigits += currentNumber % 10;
+                        currentNumber /= 10;
+                    }
+
+                    Console.WriteLine($"{Math.Abs(sumDigits)}");
+                }
+            }
+        }
+    }
+}
