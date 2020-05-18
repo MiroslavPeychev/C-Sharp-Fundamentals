a76beab93825990483baebd626c7b9c3c701a8a7
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-19 00:20:26 +0300

    Add Printing triangle

---
+++ Methods/LAB/04. Printing Triangle/PrintingTriangle.cs
@@ -0,0 +1,37 @@
+﻿namespace _04._Printing_Triangle
+{
+    using System;
+
+    public class PrintingTriangle
+    {
+        public static void Main()
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            PrintFigure(inputNumber);
+        }
+
+        public static void PrintLine(int row)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write(col + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+         public static void PrintFigure(int inputNumber)
+        {
+            for (int row = 1; row <= inputNumber; row++)
+            {
+                PrintLine(row);
+            }
+
+            for (int row = inputNumber - 1; row >= 1; row--)
+            {
+                PrintLine(row);
+            }
+        }
+    }
+}
