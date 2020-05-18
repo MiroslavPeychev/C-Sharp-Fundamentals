ae24d8073af4a32daa2629d512da3d45b2521bb4
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-19 00:19:56 +0300

    Add Calculations

---
+++ Methods/LAB/03. Calculations/Calculations.cs
@@ -0,0 +1,52 @@
+﻿namespace _03._Calculations
+{
+    using System;
+
+    public class Calculations
+    {
+        public static void Main()
+        {
+            string command = Console.ReadLine();
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            switch (command)
+            {
+                case "add":
+                    Add(firstNumber, secondNumber);
+                    break;
+                case "subtract":
+                    Subtract(firstNumber, secondNumber);
+                    break;
+                case "multiply":
+                    Multiply(firstNumber, secondNumber);
+                    break;
+                case "divide":
+                    Divide(firstNumber, secondNumber);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        private static void Subtract(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        private static void Multiply(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        private static void Divide(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+    }
+}
