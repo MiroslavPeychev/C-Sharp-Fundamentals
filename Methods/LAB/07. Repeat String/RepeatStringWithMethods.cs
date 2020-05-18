f20ca4e09852b07419f0a6af62f743f232761081
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-19 00:22:10 +0300

    Add Repeat string

---
+++ Methods/LAB/07. Repeat String/RepeatStringWithMethods.cs
@@ -0,0 +1,30 @@
+﻿namespace _07._Repeat_String
+{
+    using System;
+    using System.Text;
+
+    public class RepeatStringWithMethods
+    {
+        public static void Main()
+        {
+            string inputStr = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+
+            string result = RepeatString(inputStr, count);
+
+            Console.WriteLine(result);
+        }
+
+        private static string RepeatString(string inputStr, int count)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < count; i++)
+            {
+                result.Append(inputStr);
+            }
+
+            return result.ToString();
+        }
+    }
+}
