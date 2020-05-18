efeb968f1eb37f6a66ee49d62339580fb68ba188
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-19 00:18:10 +0300

    Add Sign of integer numbers

---
+++ Methods/LAB/01. Sign of Integer Numbers/SignOfIntegerNumbers.cs
@@ -0,0 +1,34 @@
+﻿namespace _01._Sign_of_Integer_Numbers
+{
+    using System;
+
+    public class SignOfIntegerNumbers
+    {
+        public static void Main()
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            PrintSign(inputNumber);
+        }
+
+        public static void PrintSign(int inputNumber)
+        {
+            string output = string.Empty;
+
+            if (inputNumber > 0)
+            {
+                output = $"The number {inputNumber} is positive.";
+            }
+            else if (inputNumber < 0)
+            {
+                output = $"The number {inputNumber} is negative.";
+            }
+            else
+            {
+                output = $"The number {inputNumber} is zero.";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
