5dbdc95084e532c75362d50d3ff82f1dbfab8f3f
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-20 23:45:30 +0300

    Add Multiply evens by odds

---
+++ Methods/LAB/10. Multiply Evens by Odds/MultiplyEvensByOdds.cs
@@ -0,0 +1,55 @@
+﻿
+namespace _10._Multiply_Evens_by_Odds
+{
+    using System;
+
+     public class MultiplyEvensByOdds
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int result = GetMultipleOfEvensAndOdds(Math.Abs(number));
+
+            Console.WriteLine(result);
+        }
+
+        public static int GetMultipleOfEvensAndOdds(int number)
+        {
+         //int evenNumbersSum = GetSumOfEvenDigits(number)
+         //int oddNumbersSum = GetSumOfOddDigits(number)
+         // return evenNumbersSum * oddNumbersSum;
+            return GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
+        }
+
+        public static int GetSumOfEvenDigits(int number)
+        {
+            // int everNumbersResult = GetSumOfDigits(number, 0);
+            //return everNumbersResult;
+            return GetSumOfDigits(number, 0);
+        }
+
+        public static int GetSumOfOddDigits(int number)
+        {
+            return GetSumOfDigits(number, 1);
+        }
+
+        public static int GetSumOfDigits(int number, int expectedReemainder)
+        {
+            int sum = 0;
+
+            while (number > 0)
+            {
+                int last = number % 10;
+                number /= 10;
+
+                if (last % 2 == expectedReemainder)
+                {
+                    sum += last;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
