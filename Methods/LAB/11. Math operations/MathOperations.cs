54266f16281ae284a0198ee6d95d47c72d91cf55
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-20 23:45:59 +0300

    Add Math operations

---
+++ Methods/LAB/11. Math operations/MathOperations.cs
@@ -0,0 +1,40 @@
+﻿namespace _11._Math_operations
+{
+    using System;
+
+    public class MathOperations
+    {
+        public static void Main()
+        {
+            var firstNumber = int.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            var secondNumber = int.Parse(Console.ReadLine());
+
+            double result = Calculate(firstNumber, @operator, secondNumber);
+
+            Console.WriteLine(result);
+
+        }
+        private static double Calculate(int a, string @operator, int b)
+        {
+            double result = 0;
+
+            switch (@operator)
+            {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+            }
+            return result;
+        }
+    }
+}
