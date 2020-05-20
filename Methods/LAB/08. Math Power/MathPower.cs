6faf6750f33db264e9f33e0f5f97ec58944ef33b
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-20 23:44:01 +0300

    Add Math power

---
+++ Methods/LAB/08. Math Power/MathPower.cs
@@ -0,0 +1,29 @@
+﻿namespace _08._Math_Power
+{
+    using System;
+
+    public class MathPower
+    {
+        public static void Main()
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            double result = RaiseToPower(number, power);
+
+            Console.WriteLine(result);
+        }
+
+        private static double RaiseToPower( double number,  int power)
+        {
+            double result = 1;
+
+            for (int i = 1; i <= power; i++)
+            {
+                result *= number;
+            }
+
+            return result;
+        }
+    }
+}
