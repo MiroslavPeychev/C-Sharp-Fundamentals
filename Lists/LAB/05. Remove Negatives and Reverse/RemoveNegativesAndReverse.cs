e09fbf14eba1826feae688ce3f5ff4246fb3b43d
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-25 22:12:54 +0300

    Add Remove negatives and reverse

---
+++ Lists/LAB/05. Remove Negatives and Reverse/RemoveNegativesAndReverse.cs
@@ -0,0 +1,41 @@
+﻿namespace _05._Remove_Negatives_and_Reverse
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class RemoveNegativesAndReverse
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> onlyPositiveNumbers = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int currentNumber = numbers[i];
+
+                if (currentNumber > 0)
+                {
+                    onlyPositiveNumbers.Add(currentNumber);
+                }
+            }
+
+            onlyPositiveNumbers.Reverse();
+
+            if (onlyPositiveNumbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.Write(string.Join(" ", onlyPositiveNumbers));
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
