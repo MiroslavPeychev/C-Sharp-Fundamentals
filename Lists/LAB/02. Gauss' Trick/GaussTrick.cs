5e81184c67796c4c53af6f93b11957947f749a70
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-25 22:10:01 +0300

    Add Gauss trick

---
+++ Lists/LAB/02. Gauss' Trick/GaussTrick.cs
@@ -0,0 +1,36 @@
+﻿namespace _02._Gauss__Trick
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class GaussTrick
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                var first = numbers[i];
+                var last = numbers[numbers.Count - 1 - i];
+
+                var resultNumber = first + last;
+                result.Add(resultNumber);
+            }
+
+            if (numbers.Count % 2 == 1)
+            {
+                var middleNumber = numbers[numbers.Count / 2];
+                result.Add(middleNumber);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
