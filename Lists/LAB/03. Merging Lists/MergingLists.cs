60324ebefdb9226e5c16e73dde1c2817023de8b5
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-25 22:10:57 +0300

    Add Merging lists

---
+++ Lists/LAB/03. Merging Lists/MergingLists.cs
@@ -0,0 +1,50 @@
+﻿namespace _03._Merging_Lists
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class MergingLists
+    {
+        public static void Main()
+        {
+            List<int> firstListNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondtListNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> result = new List<int>();
+
+            int minCount = Math.Min(firstListNumbers.Count, secondtListNumbers.Count);
+
+            for (int i = 0; i < minCount; i++)
+            {
+                result.Add(firstListNumbers[i]);
+                result.Add(secondtListNumbers[i]);
+            }
+
+            List<int> biggerList;
+
+            if (minCount == firstListNumbers.Count)
+            {
+                biggerList = secondtListNumbers;
+            }
+            else
+            {
+                biggerList = firstListNumbers;
+            }
+
+            for (int i = minCount; i < biggerList.Count; i++)
+            {
+                result.Add(biggerList[i]);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
