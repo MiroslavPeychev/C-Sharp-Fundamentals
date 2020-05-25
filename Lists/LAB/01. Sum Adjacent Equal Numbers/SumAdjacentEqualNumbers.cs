b00433785d32cb2ccee8a74aebeee3a257fda5ce
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-25 22:08:59 +0300

    Add Sum adjacent equal numbers

---
+++ Lists/LAB/01. Sum Adjacent Equal Numbers/SumAdjacentEqualNumbers.cs
@@ -0,0 +1,32 @@
+﻿namespace _01._Sum_Adjacent_Equal_Numbers
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class SumAdjacentEqualNumbers
+    {
+        public static void Main()
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                double currentNumber = numbers[i];
+                double nexNumber = numbers[i + 1];
+
+                if (currentNumber == nexNumber)
+                {
+                    numbers[i] *= 2;
+                    numbers.Remove(nexNumber);
+                    i = -1;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
