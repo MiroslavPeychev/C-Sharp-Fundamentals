a3a408e3e70c01ea84223ce7007a228dacce430a
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-26 20:19:16 +0300

    Add Change list

---
+++ Lists/Exercise/02. Change List/ChangeList.cs
@@ -0,0 +1,52 @@
+﻿namespace _02._Change_List
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class ChangeList
+    {
+        public static void Main()
+        {
+            List<int> input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            while (true)
+            {
+                string inputReading = Console.ReadLine();
+
+                if (inputReading == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    string[] tokens = inputReading.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    if (tokens[0] == "Delete")
+                    {
+                        int toDelete = int.Parse(tokens[1]);
+
+                        for (int i = 0; i < input.Count; i++)
+                        {
+                            if (input[i] == toDelete)
+                            {
+                                input.Remove(input[i]);
+                            }
+                        }
+                    }
+                    else if (tokens[0] == "Insert")
+                    {
+                        int numberToAdd = int.Parse(tokens[1]);
+                        int possition = int.Parse(tokens[2]);
+                        input.Insert(possition, numberToAdd);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+    }
+}
