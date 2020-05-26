b1eca98b3e10b2f994fe05006b8dc30cf943e323
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-26 20:27:05 +0300

    Add Lists - Exercise.csproj

---
+++ Lists/Exercise/Lists - Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lists___Exercise
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
