735c7a68e23b5b49b6eb13d31ec57459a90bf174
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-26 20:18:41 +0300

    Add Train

---
+++ Lists/Exercise/01. Train/Train.cs
@@ -0,0 +1,56 @@
+﻿namespace _01._Train
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Train
+    {
+        public static void Main()
+        {
+            List<int> wagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int maxWagonCapacity = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                string commandLine = Console.ReadLine();
+
+                if (commandLine == "end")
+                {
+                    break;
+                }
+
+                string[] tokens = commandLine.Split();
+
+                string command = tokens[0];
+
+
+                if (command == "Add")
+                {
+                    int wagonToAdd = int.Parse(tokens[1]);
+                    wagons.Add(wagonToAdd);
+                }
+                else
+                {
+                    int passengersToAdd = int.Parse(tokens[0]);
+
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        int currentElement = wagons[i];
+                        if (currentElement + passengersToAdd <= maxWagonCapacity)
+                        {
+                            wagons[i] += passengersToAdd;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
