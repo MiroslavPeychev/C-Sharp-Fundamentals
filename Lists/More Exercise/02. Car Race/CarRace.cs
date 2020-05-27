1dae9113110c09198bbc9fa9b270ab08fb42dde2
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-27 21:41:28 +0300

    Add Car race

---
+++ Lists/More Exercise/02. Car Race/CarRace.cs
@@ -0,0 +1,52 @@
+﻿namespace _02._Car_Race
+{
+    using System;
+    using System.Linq;
+
+    public class CarRace
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var firstRacer = numbers;
+            var secondRacer = numbers
+                .Reverse()
+                .ToArray();
+
+            double sumFirst = SumNumbers(firstRacer);
+            double sumSecond = SumNumbers(secondRacer);                      
+
+            if (sumFirst < sumSecond)
+            {
+                Console.WriteLine($"The winner is left with total time: {sumFirst}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {sumSecond}");
+            }            
+        }
+
+        private static double SumNumbers(int[] racer)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < racer.Length / 2; i++)
+            {                
+                    if (racer[i] == 0)
+                    {
+                        sum *= 0.8;
+                    }
+                    else
+                    {
+                        sum += racer[i];
+                    }                
+            }
+
+            return sum;
+        }
+    }
+}
