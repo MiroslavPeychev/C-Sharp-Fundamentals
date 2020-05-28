98954119e87bad61888eda9263a0aab1470eb284
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-28 21:37:11 +0300

    Add Middle characters

---
+++ Methods/Exercise/06. Middle Characters/MiddleCharacter.cs
@@ -0,0 +1,24 @@
+﻿namespace _06._Middle_Characters
+{
+    using System;
+
+    public class MiddleCharacter
+    {
+        public static void Main()
+        {
+            string inputString = Console.ReadLine();
+            string subStr;
+
+            if (inputString.Length % 2 == 0)
+            {
+                subStr = inputString.Substring(inputString.Length / 2 - 1, 2);
+            }
+            else
+            {
+                subStr = inputString.Substring(inputString.Length / 2, 1);
+            }
+
+            Console.WriteLine(subStr);
+        }
+    }
+}
