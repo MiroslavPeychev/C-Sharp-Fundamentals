00fde12a7e093a39cdfdcd1ad626af5f46c10550
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-28 21:39:40 +0300

    Add Top numbers

---
+++ Methods/Exercise/10. Top Number/TopNumber.cs
@@ -0,0 +1,61 @@
+﻿namespace _10._Top_Number
+{
+    using System;
+
+    public class TopNumber
+    {
+        public static void Main()
+        {
+            int range = int.Parse(Console.ReadLine());
+
+            for (int i = 8; i < range; i++)
+            {
+                int num = i;
+
+                bool isDivisibleBy8 = CheckIsDivisibleBy8(i);
+
+                bool containsOddNumber = ContainsOddNumber(i);
+
+                if (isDivisibleBy8 && containsOddNumber)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static bool ContainsOddNumber(int number)
+        {
+            while (number != 0)
+            {
+                int digit = number % 10;
+                number /= 10;
+
+                if (digit % 2 == 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool CheckIsDivisibleBy8(int number)
+        {
+            int sum = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+                number /= 10;
+                sum += digit; 
+            }
+
+            if (sum % 8 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
