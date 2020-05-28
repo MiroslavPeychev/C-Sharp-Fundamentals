f2863003ce7c89c4ab8ceeefcab129a8748b591c
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-28 21:38:56 +0300

    Add Palindrome integers

---
+++ Methods/Exercise/09. Palindrome Integers/PalindromeIntegers.cs
@@ -0,0 +1,36 @@
+﻿namespace _09._Palindrome_Integers
+{
+    using System;
+
+    public class PalindromeIntegers
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                string word = Console.ReadLine();
+
+                if (word == "END")
+                {
+                    break;
+                }
+
+                string reversedWord = string.Empty;
+
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reversedWord += word[i];
+                }
+
+                bool isPalindrome = IsPalindrome(word, reversedWord);
+
+                Console.WriteLine(isPalindrome == true ? "true" : "false");
+            }
+        }
+
+        private static bool IsPalindrome(string word, string reversedWord)
+        {
+            return word == reversedWord;
+        }
+    }
+}
