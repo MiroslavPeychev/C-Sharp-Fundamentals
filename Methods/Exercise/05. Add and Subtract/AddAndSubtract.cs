27966e46fa97ac7791b7a4a8478048d1a2aa14be
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-28 21:36:36 +0300

    Add Add and subtract

---
+++ Methods/Exercise/05. Add and Subtract/AddAndSubtract.cs
@@ -0,0 +1,27 @@
+﻿namespace _05._Add_and_Subtract
+{
+    using System;
+
+    public class AddAndSubtract
+    {
+        public static void Main()
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int sumNumbers = SumTwoNumbers(firstNumber, secondNumber);
+
+            int result = sumNumbers - thirdNumber;
+
+            Console.WriteLine(result);
+        }
+
+        private static int SumTwoNumbers(int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+
+            return sum;
+        }
+    }
+}
