b6f75621d782b86eb5e909a4876779251e2d473f
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-28 21:37:51 +0300

    Add NxN matrix

---
+++ Methods/Exercise/07. NxN Matrix/NxNMatrix.cs
@@ -0,0 +1,27 @@
+﻿namespace _07._NxN_Matrix
+{
+    using System;
+
+    public class NxNMatrix
+    {
+        public static void Main()
+        {
+            int sizeMatrix = int.Parse(Console.ReadLine());
+
+            PrintMatrix(sizeMatrix);
+        }
+
+        static void PrintMatrix(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(size + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
