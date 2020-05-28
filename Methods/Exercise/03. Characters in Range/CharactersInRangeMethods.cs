b1ee5e9ff24b2e52f56b45961abd5da22d00136c
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-28 21:33:24 +0300

    Add Characters in range

---
+++ Methods/Exercise/03. Characters in Range/CharactersInRangeMethods.cs
@@ -0,0 +1,28 @@
+﻿namespace _03._Characters_in_Range
+{
+    using System;
+
+    public class CharactersInRangeMethods
+    {
+        public static void Main()
+        {
+            char startCharacter = char.Parse(Console.ReadLine());
+            char endCharacter = char.Parse(Console.ReadLine());
+
+            string result = CharactersInRange(startCharacter, endCharacter);
+
+            Console.WriteLine(result);
+        }
+        public static string CharactersInRange(char startCharacter, char endCharacter)
+        {
+            string result = string.Empty;
+
+            for (int i = Math.Min(startCharacter, endCharacter) + 1; i < Math.Max(startCharacter, endCharacter); i++)
+            {
+                result += ((char)i + " ");
+            }
+
+            return result;
+        }
+    }
+}
