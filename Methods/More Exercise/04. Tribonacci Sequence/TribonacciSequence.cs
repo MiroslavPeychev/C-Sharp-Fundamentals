126d4baa103ddd5de7e828958dd889a2de03d3dc
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-05-29 20:33:49 +0300

    Add Tribonacci sequence

---
+++ Methods/More Exercise/04. Tribonacci Sequence/TribonacciSequence.cs
@@ -0,0 +1,34 @@
+﻿namespace _04._Tribonacci_Sequence
+{
+    using System;
+
+    public class TribonacciSequence
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write(Tribonacci(i) + " ");
+            }
+        }
+
+        public static long Tribonacci(int number)
+        {
+            long firstNum = 1;
+            long secondNum = 1;
+            long thirdNum = 2;
+
+            for (int i = 1; i < number; i++)
+            {
+                long temp = firstNum;
+                firstNum = secondNum;
+                secondNum = thirdNum;
+                thirdNum = temp + firstNum + secondNum;
+            }
+
+            return firstNum;
+        }
+    }
+}
