09fb8a19b03db3ae386b233d30a2b209122b393b
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-01 21:40:20 +0300

    Add Count real numbers

---
+++ Associative Arrays/LAB/01. Count Real Numbers/CountRealNumbers.cs
@@ -0,0 +1,35 @@
+﻿namespace _01._Count_Real_Numbers
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class CountRealNumbers
+    {
+        public static void Main()
+        {
+            int[] nums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            SortedDictionary<int, int> counts = new SortedDictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+            }
+            foreach (KeyValuePair<int, int> num in counts)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+        }
+    }
+}
