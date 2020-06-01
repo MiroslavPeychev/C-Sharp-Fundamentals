40224ab989100b60f60a9e122df4ddcbc20c4313
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-01 21:41:48 +0300

    Add Odd occurrences

---
+++ Associative Arrays/LAB/02. Odd Occurrences/OddOccurrences.cs
@@ -0,0 +1,35 @@
+﻿namespace _02._Odd_Occurrences
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class OddOccurrences
+    {
+        public static void Main()
+        {
+            string[] words = Console.ReadLine().Split();
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string wordInLowerCase = word.ToLower();
+                if (counts.ContainsKey(wordInLowerCase))
+                {
+                    counts[wordInLowerCase]++;
+                }
+                else
+                {
+                    counts.Add(wordInLowerCase, 1);
+                }
+            }
+
+            foreach (KeyValuePair<string, int> count in counts)
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
