38567f6343a21d2cc1917735974547b5df64c888
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-02 22:06:18 +0300

    Add Company users

---
+++ Associative Arrays/Exercise/08. Company Users/CompanyUsers.cs
@@ -0,0 +1,51 @@
+﻿namespace _08._Company_Users
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class CompanyUsers
+    {
+        public static void Main()
+        {
+            Dictionary <string, List<string>>companyAndEmployees = new Dictionary<string, List<string>>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split("->");
+
+                string companyName = tokens[0];
+                string employeeId = tokens[1];
+
+                if (!companyAndEmployees.ContainsKey(companyName))
+                {
+                    companyAndEmployees[companyName] = new List<string>();
+                }
+
+                if (!companyAndEmployees[companyName].Contains(employeeId))
+                {
+                    companyAndEmployees[companyName].Add(employeeId);
+                }
+            }
+
+            foreach (KeyValuePair<string, List<string>> kvp in companyAndEmployees.OrderBy(x => x.Key))
+            {
+                string companyName = kvp.Key;
+                List<string> employees = kvp.Value;
+
+                Console.WriteLine($"{companyName}");
+
+                foreach (var employeeId in employees)
+                {
+                    Console.WriteLine($"--{employeeId}");
+                }
+            }
+        }
+    }
+}
