385223f3c5592d9d7fd99fe6ce7125e23948e518
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-02 21:59:39 +0300

    Add Count chars in a string

---
+++ Associative Arrays/Exercise/01. Count Chars in a String/CountCharsInAString.cs
@@ -0,0 +1,41 @@
+﻿namespace _01._Count_Chars_in_a_String
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class CountCharsInAString
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> lettersCount = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char letter = input[i];
+
+                if (letter != ' ')
+                {
+                    if (!lettersCount.ContainsKey(letter))
+                    {
+                        lettersCount[letter] = 1; 
+                        
+                    }
+                    else                          
+                    {
+                        lettersCount[letter]++;
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<char, int> kvp in lettersCount)
+            {
+                char letter = kvp.Key;
+                int count = kvp.Value;
+
+                Console.WriteLine($"{letter} -> {count}");
+            }
+        }
+    }
+}
