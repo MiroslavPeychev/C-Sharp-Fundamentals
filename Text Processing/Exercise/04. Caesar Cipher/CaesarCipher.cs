79c0965b47336681e69581f83763ec38288452f8
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-05 22:37:59 +0300

    Add Caesar cipher

---
+++ Text Processing/Exercise/04. Caesar Cipher/CaesarCipher.cs
@@ -0,0 +1,25 @@
+﻿namespace _04._Caesar_Cipher
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class CaesarCipher
+    {
+        public static void Main()
+        {
+            string inputMessage = Console.ReadLine();
+            List<char> chars = inputMessage.ToList();
+            List<char> encryptedMessage = new List<char>();
+
+            for (int i = 0; i < chars.Count; i++)
+            {
+                int currentCharAsANumber = (int)(chars[i]);
+                char newChar = (char)(currentCharAsANumber + 3);
+                encryptedMessage.Add(newChar);
+            }
+
+            Console.WriteLine(string.Join("", encryptedMessage));
+        }
+    }
+}
