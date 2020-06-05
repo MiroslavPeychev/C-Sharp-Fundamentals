b755ee56c6b72b99955d705f42bd6734854c8067
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-05 22:39:17 +0300

    Add Replace repeating chars

---
+++ Text Processing/Exercise/06. Replace Repeating Chars/ReplaceRepeatingChars.cs
@@ -0,0 +1,26 @@
+﻿namespace _06._Replace_Repeating_Chars
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class ReplaceRepeatingChars
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            List<char> chars = input.ToList();
+
+            for (int i = 1; i < chars.Count; i++)
+            {
+                if (chars[i] == chars[i - 1])
+                {
+                    chars.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(string.Join("", chars));
+        }
+    }
+}
