e8eb94574e605f71e905fd2d941dfd2ebfec730e
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-06 23:00:48 +0300

    Add Match phone number

---
+++ Regular Expressions/LAB/02. Match Phone Number/MatchPhoneNumber.cs
@@ -0,0 +1,25 @@
+﻿namespace _02._Match_Phone_Number
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    public class MatchPhoneNumber
+    {
+        public static void Main()
+        {
+            string pattern = @"(\+359([ -])2(\2)(\d{3})(\2)(\d{4}))\b";
+            string phones = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(phones, pattern);
+            List<string> result = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                result.Add(match.ToString());
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
