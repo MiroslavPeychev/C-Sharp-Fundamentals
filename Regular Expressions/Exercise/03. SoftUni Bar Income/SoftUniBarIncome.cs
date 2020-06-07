edfc9766745b7b8793bd7134cdfa7d23bee2e1a4
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-07 17:55:43 +0300

    Add SoftUni bar income

---
+++ Regular Expressions/Exercise/03. SoftUni Bar Income/SoftUniBarIncome.cs
@@ -0,0 +1,40 @@
+﻿namespace _03._SoftUni_Bar_Income
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class SoftUniBarIncome
+    {
+        public static void Main()
+        {
+            string pattern = @"^%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.]*?(?<price>[-+]?[0-9]*\.?[0-9]+)\$";
+            double totalIncome = 0;
+
+            while (true)
+            {
+                string inputLines = Console.ReadLine();
+
+                if (inputLines == "end of shift")
+                {
+                    break;
+                }
+
+                if (Regex.IsMatch(inputLines, pattern))
+                {
+                    Match match = Regex.Match(inputLines, pattern);
+                    string customerName = match.Groups["customer"].Value;
+                    string productName = match.Groups["product"].Value;
+                    int productQuantity = int.Parse(match.Groups["count"].Value);
+                    double productPrice = double.Parse(match.Groups["price"].Value);
+                    double customerBill = productQuantity * productPrice;
+
+                    Console.WriteLine($"{customerName}: {productName} - {customerBill:f2}");
+
+                    totalIncome += customerBill;
+                }
+            }
+
+            Console.WriteLine($"Total income: {totalIncome:f2}");
+        }
+    }
+}
