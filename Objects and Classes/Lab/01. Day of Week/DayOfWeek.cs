489e0206ecb4b3cc100bb070cb25b1028dc6c310
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-09 23:40:50 +0300

    Add Day of week

---
+++ Objects and Classes/Lab/01. Day of Week/DayOfWeek.cs
@@ -0,0 +1,17 @@
+﻿namespace _01._Day_of_Week
+{
+    using System;
+    using System.Globalization;
+
+    public class DayOfWeek
+    {
+        public static void Main()
+        {
+            string dateAsText = Console.ReadLine();
+
+            DateTime dateTime = DateTime.ParseExact(dateAsText, "dd-M-yyyy", CultureInfo.InvariantCulture);
+
+            Console.WriteLine(dateTime.DayOfWeek);
+        }
+    }
+}
