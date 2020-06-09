51c78785b21932bfea66c50dd015a2ad6ad0c9af
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-09 23:42:02 +0300

    Add Randomize Words

---
+++ Objects and Classes/Lab/02. Randomize Words/RandomiseWords.cs
@@ -0,0 +1,24 @@
+﻿namespace _02._Randomize_Words
+{
+    using System;
+
+    public class RandomiseWords
+    {
+        public static void Main()
+        {
+            string[] words = Console.ReadLine().Split(' ');
+
+            Random random = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIndex = random.Next(words.Length);
+                string tempWord = words[i];
+                words[i] = words[randomIndex];
+                words[randomIndex] = tempWord;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
