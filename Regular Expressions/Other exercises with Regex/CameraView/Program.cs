93f0a41383aa68f895ff442619801855d229aeb3
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-10 22:22:12 +0300

    Add Camera view

---
+++ Regular Expressions/Other exercises with Regex/CameraView/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CameraView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"\|<(?<picture>\w+)");
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int skipNumber = numbers[0];
+            int takeNumber = numbers[1];
+
+            string text = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(text);
+
+            string[] allResults = new string[matches.Count];
+            int index = 0;
+
+            foreach (Match match in matches)
+            {
+                string currentMatch = match.Groups["picture"].Value;
+
+                char[] resultChars = currentMatch.Skip(skipNumber).Take(takeNumber).ToArray();
+
+                string currentResult = string.Join("", resultChars);
+                allResults[index++] = currentResult;
+            }
+
+            Console.WriteLine(string.Join(", ", allResults));
+
+        }
+    }
+}
