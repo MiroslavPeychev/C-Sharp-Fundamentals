79a90ef985f6ac8de92825cbcd91cd484b879e77
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-10 22:25:27 +0300

    Add Extract sentences by keyword

---
+++ Regular Expressions/Other exercises with Regex/Extract Sentences by Keyword/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Extract_Sentences_by_Keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchWord = Console.ReadLine();
+
+            string pattern = $@"\b{searchWord}\b";
+            Regex regex = new Regex(pattern);
+
+            string[] words = Console.ReadLine().Split(new char[] { '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+
+            foreach (var word in words)
+            {
+                if(regex.IsMatch(word))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
