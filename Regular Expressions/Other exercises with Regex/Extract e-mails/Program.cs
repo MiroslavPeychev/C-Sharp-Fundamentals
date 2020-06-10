19f701ccc76e096bf68fd4a524f3cf7cd1516a7d
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-10 22:24:42 +0300

    Add Extract e-mails

---
+++ Regular Expressions/Other exercises with Regex/Extract e-mails/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Extract_e_mails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<= )[A-Za-z0-9]+([.\-_][A-Za-z0-9]+)*@[A-Za-z]+(\-[A-Za-z]+)*(\.[A-Za-z]+)+";
+
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            }
+
+        }
+    }
+}
