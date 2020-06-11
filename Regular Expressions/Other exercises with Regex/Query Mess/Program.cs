3b5897ada2ae45a751b83f94fb641779ce513085
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-11 23:02:47 +0300

    Add Query mess

---
+++ Regular Expressions/Other exercises with Regex/Query Mess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Query_Mess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
