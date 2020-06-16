eb168b249d951e073a23c5f243a76d0848fd26aa
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-06-16 21:35:26 +0300

    Update

---
+++ Regular Expressions/Other exercises with Regex/Extract Sentences by Keyword/ExtractSentencesByKeyword.cs
@@ -0,0 +1,29 @@
+﻿namespace Extract_Sentences_by_Keyword
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Linq;
+    public class ExtractSentencesByKeyword
+    {
+        public static void Main()
+        {
+            string searchWord = Console.ReadLine();
+
+            string pattern = $@"\b{searchWord}\b";
+            Regex regex = new Regex(pattern);
+
+            string[] words = Console.ReadLine()
+                .Split(new char[] { '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .ToArray();
+
+            foreach (string word in words)
+            {
+                if(regex.IsMatch(word))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
