52e64eabb48fa5f822decf5436f8b9d12a2ba97d
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-08-04 15:29:40 +0300

    Add Programming Fundamentals Final Exam - 07 December 2019 Group 1

---
+++ Programming Fundamentals Final Exam - 07 December 2019 Group 1/01. Email Validator/EmailValidator.cs
@@ -0,0 +1,81 @@
+﻿namespace _01._Email_Validator
+{
+    using System;
+    using System.Linq;
+
+    public class EmailValidator
+    {
+        public static void Main()
+        {
+            string email = Console.ReadLine();
+
+            while (true)
+            {
+                string inputCommand = Console.ReadLine();
+                string[] commands = inputCommand.Split();
+
+                string command = commands[0];
+
+                if (command == "Complete")
+                {
+                    break;
+                }
+
+                if (command == "Make")
+                {
+                    if (commands[1] == "Upper")
+                    {
+                        email = email.ToUpper();
+                        Console.WriteLine(email);
+                    }
+                    else if (commands[1] == "Lower")
+                    {
+                        email = email.ToLower();
+                        Console.WriteLine(email);
+                    }
+                }
+                else if (command == "GetDomain")
+                {
+                    int count = int.Parse(commands[1]);
+
+                    string domain = email.Substring(email.Length - count);//email.Length - count, count
+                    Console.WriteLine(domain);
+                }
+                else if (command == "GetUsername")
+                {
+                    if (email.Contains("@"))
+                    {
+                        int index = email.IndexOf('@');
+                        string username = email.Substring(0, index);
+                        Console.WriteLine(username);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The email {email} doesn't contain the @ symbol.");
+                    }
+                }
+                else if (command == "Replace")
+                {
+                    char charToReplace = char.Parse(commands[1]);
+
+                    if (email.Contains(charToReplace))
+                    {
+                        email = email.Replace(charToReplace, '-');                        
+                    }
+
+                    Console.WriteLine(email);
+                }
+
+                else if (command == "Encrypt")
+                {
+                    for (int i = 0; i < email.Length; i++)
+                    {
+                        Console.Write($"{(int)email[i]} ");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals Final Exam - 07 December 2019 Group 1/Programming Fundamentals Final Exam - 07 December 2019 Group 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programming_Fundamentals_Final_Exam___07_December_2019_Group_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
