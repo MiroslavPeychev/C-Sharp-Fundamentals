2a41c7e89002fd6fa731c47a6d30d0b1262966c6
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-08-06 23:38:05 +0300

    Add MeTube Statistics

---
+++ Associative Arrays/More tasks with Dictionaries/MeTube Statistics/StartUp.cs
@@ -0,0 +1,85 @@
+﻿
+namespace MeTube_Statistics
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var videoAndViews = new Dictionary<string, int>();
+            var videoAndLikesOrDislikes = new Dictionary<string, long>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "stats time")
+                {
+                    break;
+                }
+
+                if (input.Contains('-'))
+                {
+                    string[] tokens = input.Split("-");
+                    string videoName = tokens[0];
+                    int views = int.Parse(tokens[1]);
+
+                    if (!videoAndViews.ContainsKey(videoName))
+                    {
+                        videoAndViews[videoName] = 0;
+                    }
+                    videoAndViews[videoName] += views;
+
+                    if (!videoAndLikesOrDislikes.ContainsKey(videoName))
+                    {
+                        videoAndLikesOrDislikes[videoName] = 0;
+                    };
+                }
+                else if (input.Contains(':'))
+                {
+                    string[] tokens = input.Split(":");
+                    string likeOrDislike = tokens[0];
+                    string videoName = tokens[1];
+
+                    if (likeOrDislike == "like")
+                    {
+                        if (videoAndLikesOrDislikes.ContainsKey(videoName))
+                        {
+                            videoAndLikesOrDislikes[videoName] += 1;
+                        }
+                    }
+                    else if (likeOrDislike == "dislike")
+                    {
+                        if (videoAndLikesOrDislikes.ContainsKey(videoName))
+                        {
+                            videoAndLikesOrDislikes[videoName] -= 1;
+                        }
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            if (command == "by views")
+            {
+                foreach (var kvp in videoAndViews.OrderByDescending(x => x.Value))
+                {
+                    long likes = videoAndLikesOrDislikes[kvp.Key];
+
+                    Console.WriteLine($"{kvp.Key} - {kvp.Value} views - {likes} likes");
+                }
+            }
+            else if (command == "by likes")
+            {
+                foreach (var kvp in videoAndLikesOrDislikes.OrderByDescending(x => x.Value))
+                {
+                    int views = videoAndViews[kvp.Key];
+                    Console.WriteLine($"{kvp.Key} - {views} views - {kvp.Value} likes");
+                }
+            }
+        }
+    }
+}
