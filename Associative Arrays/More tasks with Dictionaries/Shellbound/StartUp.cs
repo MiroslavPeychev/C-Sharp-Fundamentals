29cf96aa926ae7d3b8b45e6bdd22481b39580a46
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-08-07 21:05:37 +0300

    Add Shellbound

---
+++ Associative Arrays/More tasks with Dictionaries/Shellbound/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace Shellbound
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var home = new Dictionary<string, List<int>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "Aggregate")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split();
+                string location = tokens[0];
+                int shells = int.Parse(tokens[1]);
+
+                if (!home.ContainsKey(location))
+                {
+                    home.Add(location, new List<int>());
+                }
+
+                if (!home[location].Contains(shells))
+                {
+                    home[location].Add(shells);
+                }
+            }
+
+            foreach (var kvp in home)
+            {
+                string location = kvp.Key;
+                List<int> shells = kvp.Value;
+
+                var average = shells.Average();
+                var sum = shells.Sum();
+                var result = Math.Ceiling(sum - average);
+                Console.WriteLine($"{location} -> {string.Join(", ", shells)} ({result})");
+            }
+        }
+    }
+}
