3f2751be720e733bc14a2b194c6116289e05ada7
Miroslav Peychev <MiroslavPeychev@mail.bg>
2020-08-12 18:50:30 +0300

    Add Periodic Table

---
+++ Associative Arrays/More tasks with Dictionaries/Periodic Table/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace Periodic_Table
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            var list = new HashSet<string>();
+
+            for (int i = 0; i < number; i++)
+            {
+                var line = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var item in line)
+                {
+                    list.Add(item);
+                }
+
+            }
+
+            Console.WriteLine(string.Join(" ", list.OrderBy(x => x)));
+        }
+    }
+}
